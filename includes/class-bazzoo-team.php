72e10df5334b140c2c3a3d70c6da4745abe5d10b
Dominic Talbot <dominic.talbot@me.com>
2014-12-31 10:52:00 +0000

    Merged the CSS and have the register post type function working

--- includes/class-bazzoo-team.php
+++ includes/class-bazzoo-team.php
@@ -109,6 +109,49 @@ class Bazzoo_Team {
 			$this->admin = new Bazzoo_Team_Admin_API();
 		}
 
+		add_action( 'init', 'register_cpt_team_members' );
+		// Create the Post Type
+		function register_cpt_team_members() {
+
+		    $labels = array( 
+		        'name' => _x( 'Team Members', 'team_members' ),
+		        'singular_name' => _x( 'Team Member', 'team_members' ),
+		        'add_new' => _x( 'Add New', 'team_members' ),
+		        'add_new_item' => _x( 'Add New Team Member', 'team_members' ),
+		        'edit_item' => _x( 'Edit Team Member', 'team_members' ),
+		        'new_item' => _x( 'New Team Member', 'team_members' ),
+		        'view_item' => _x( 'View Team Member', 'team_members' ),
+		        'search_items' => _x( 'Search Team Members', 'team_members' ),
+		        'not_found' => _x( 'No team members found', 'team_members' ),
+		        'not_found_in_trash' => _x( 'No team members found in Trash', 'team_members' ),
+		        'parent_item_colon' => _x( 'Parent Team Member:', 'team_members' ),
+		        'menu_name' => _x( 'Team Members', 'team_members' ),
+		    );
+
+		    $args = array( 
+		        'labels' => $labels,
+		        'hierarchical' => true,
+		        
+		        'supports' => array( 'title', 'editor', 'author', 'thumbnail', 'trackbacks', 'custom-fields', 'revisions', 'page-attributes' ),
+		        
+		        'public' => true,
+		        'show_ui' => true,
+		        'show_in_menu' => true,
+		        
+		        
+		        'show_in_nav_menus' => false,
+		        'publicly_queryable' => true,
+		        'exclude_from_search' => false,
+		        'has_archive' => true,
+		        'query_var' => true,
+		        'can_export' => true,
+		        'rewrite' => true,
+		        'capability_type' => 'post'
+		    );
+
+		    register_post_type( 'team_members', $args );
+		}
+
 		// Handle localisation
 		$this->load_plugin_textdomain();
 		add_action( 'init', array( $this, 'load_localisation' ), 0 );
